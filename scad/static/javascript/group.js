a68890785d99143180aec8afc7b9bd9d8fc755c2
sellylu <s83721456@gmail.com>
2016-01-05 14:49:43 +0800

    fix bug: duplicated declared function

--- scad/static/javascript/group.js
+++ scad/static/javascript/group.js
@@ -1,35 +1,7 @@
-$.ajaxSetup({
-    data: {csrfmiddlewaretoken: '{{ csrf_token }}' },
-});
-
-$(document).ready(function() {
-				  checkShowLoginDiv();
-				  setuser_no();
-				  checkShowAddButton('{{group_page_data.group_member}}');
-				  showprogress('{{group_page_data.created_time}}','{{ group_page_data.finished_time}}');
-				  showNews('{{group_page_data.group_id}}')
-				  });
-
-function checkShowLoginDiv() {
-    user_id = Cookies.get('user_id');
-	if(user_id != undefined)
-		adjustCSS();
-}
-
 function adjustCSS() {
 	$('#navbar-button').show();
 	$('#create_group_button').show();
 	$('#navbar-login').hide();
-}
-
-function getMyInfoURL(){
-	user_id = Cookies.get('user_id');
-	window.location = '/user/'+user_id+'/';
-}
-
-function logout() {
-	Cookies.remove('user_id');
-	window.location = '/';
 }
 
 function showprogress(created_time,finish_time){
@@ -51,52 +23,6 @@ function showprogress(created_time,finish_time){
 		str = 'width:' + perc + '%';
 		$('#progress-bar').attr('style',str);
 	}
-}
-
-// Create Group
-function creategroup_submit() {
-	check_group_name = $('#group_name').val();
-	check_group_intro = $('#intro').val();
-	check_time = $('#finished_time_date').val();
-	nosubmit = 0;
-	if(check_time == '') {
-		$('#finished_time_date').attr('style','border: 1px solid red');
-		nosubmit =1;
-	} else {
-		$('#finished_time_date').removeAttr('style');
-	}
-	if(check_group_intro =='') {
-		$('#introdiv').attr('class','form-group has-error');
-		nosubmit =1;
-	} else {
-		$('#introdiv').attr('class','form-group');
-	}
-	if(check_group_name=='') {
-		$('#namediv').attr('class','form-group has-error');
-		nosubmit =1;
-	} else {
-		$('#namediv').attr('class','form-group');
-	}
-	if(nosubmit==1)return false;
-
-	creator_id = Cookies.get('user_id');
-	group_name = document.getElementById("group_name").value;
-	intro = document.getElementById("intro").value;
-	finished_time = document.getElementById("finished_time_date").value;
-	if(document.getElementById("private_op1").checked) {
-		private = 0;
-	} else {
-		private = 1;
-	}
-	date = Date.now();
-	group_id = creator_id + date;
-	member_limit = parseInt(document.getElementsByName("member_limit")[0].value);
-
-	$.post( "/", { group_id : group_id, group_name : group_name,  member_limit :member_limit,intro:intro,private:private,creator_id:creator_id ,finished_time:finished_time})
-		.then(function () {
-			window.location = '/group/' + group_id;
-	});
-	// TODO: display link of the group
 }
 
 // News Tab
@@ -314,64 +240,3 @@ function checkShowAddButton(member){
 
     }
 }
-
-function saveUserInfo() {
-	FB.api('/me',{"fields": "name, email"}, function(response) {
-		if(response && !response.error) {
-			$.post("/",{
-				user_id : response.id, user_name: response.name, user_email: response.email})
-				.then(function(){
-					adjustCSS();
-					Cookies.set('user_id',response.id);
-					console.log('Successful login for: ' + response.name + ' with ' + response.id + ' and ' + response.email);
-				});
-		}
-	});
-}
-
-// Facebook
-(function(d, s, id){
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement(s); js.id = id;
- js.src = "http://connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-                      statusChangeCallback(response);
-                      });
-}
-
-function statusChangeCallback(response) {
-    if (response.status === 'connected') {
-        saveUserInfo();
-    } else if (response.status === 'not_authorized') {
-            FB.login(function(response) {
-                    if (response.authResponse) {
-                        FB.api('/me', function (response) {
-                            saveUserInfo();
-                        });
-                    }
-            }, {scope: 'email'});
-    } else {
-        alert('Please log ' + 'into Facebook.')
-        FB.login(function(response) {
-                    if (response.authResponse) {
-                        FB.api('/me', function(response) {
-                            saveUserInfo();
-                         });
-                    }
-        }, {scope: 'email'});
-    }
-}
-
-window.fbAsyncInit = function() {
-    FB.init({
-            appId      : '903468286374795',
-			cookie     : true,
-            xfbml      : true,
-            version    : 'v2.5'
-            });
-};
