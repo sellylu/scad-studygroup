a9929f923610926212da2a5e0d047f85520c0a10
sellylu <s83721456@gmail.com>
2016-01-10 17:18:01 +0800

    fix facebook app id

--- StudyGroup/static/javascript/index.js
+++ StudyGroup/static/javascript/index.js
@@ -125,7 +125,7 @@ function statusChangeCallback(response) {
 
 window.fbAsyncInit = function() {
     FB.init({
-            appId      : '903468286374795',
+            appId      : '444916912380076',
 			cookie     : true,
             xfbml      : true,
             version    : 'v2.5'
